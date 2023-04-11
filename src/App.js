18bb92efae2b530f309063cca119fcf6bcd4b09b
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-11 21:28:02 +0300

    Initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto" >
+          
+        </Box>
+    );
+};
+
+export default App;
