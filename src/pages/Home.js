76841212227bebac771b21d145b1151d455261e3
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-12 11:07:50 +0300

    Added HeroBanner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Box } from '@mui/material';
 
+import HeroBanner from '../components/HeroBanner';
+
 const Home = () => {
 
     return (
         <Box>
-           
+            <HeroBanner />
         </Box>
     );
 };
