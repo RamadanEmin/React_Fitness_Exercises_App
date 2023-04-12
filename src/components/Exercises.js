337c267f76bed6cb6d3195064f92ff9d88292463
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-12 23:30:22 +0300

    Fetch data from ExerciseDB API

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
     const [currentPage, setCurrentPage] = useState(1);
@@ -10,6 +12,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         setCurrentPage(value);
         window.scrollTo({ top: 1800, behavior: 'smooth' });
     };
+
+    useEffect(() => {
+        const fetchExercisesData = async () => {
+            let exercisesData = [];
+
+            if (bodyPart === 'all') {
+                exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+            } else {
+                exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+            }
+
+            setExercises(exercisesData);
+        };
+
+        fetchExercisesData();
+    }, [bodyPart]);
 
     return (
         <Box id="exercises"
