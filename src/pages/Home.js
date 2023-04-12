3f8754ded42bde35ef9a14a79e960ae19a008fc4
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-12 11:48:47 +0300

    Added SearchExercises component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
 import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
+    const [bodyPart, setBodyPart] = useState('all');
 
     return (
         <Box>
             <HeroBanner />
+            <SearchExercises bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
     );
 };
