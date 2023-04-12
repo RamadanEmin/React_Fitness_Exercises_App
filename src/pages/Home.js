734e541ab88cf7752c110e4baa45e12d2d9e9190
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-12 21:49:32 +0300

    Added Exercises component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,17 @@ import { Box } from '@mui/material';
 
 import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
 
 const Home = () => {
     const [bodyPart, setBodyPart] = useState('all');
+    const [exercises, setExercises] = useState([]);
 
     return (
         <Box>
             <HeroBanner />
-            <SearchExercises bodyPart={bodyPart} setBodyPart={setBodyPart} />
+            <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+            <Exercises exercises={exercises} setExercises={setExercises} bodyPart={bodyPart} />
         </Box>
     );
 };
