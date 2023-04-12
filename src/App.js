966d7b24b559644860a7b268801ce6e86921c78b
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-12 10:09:18 +0300

    Added 'Navbar' component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Box } from '@mui/material';
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
         <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto" >
-          
+            <Navbar />
         </Box>
     );
 };
