2f98500562cef0e36c73f537197f529171401273
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-12 10:46:28 +0300

    Added Home page and Footer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
         <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto" >
             <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+            <Footer />
         </Box>
     );
 };
