389e365c4e7c6df3fbbd78c21b1146e07efc0bd5
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-13 11:59:00 +0300

    Added ExerciseCard component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,10 +3,15 @@ import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const exercisesPerPage = 9;
+
+    const indexOfLastExercise = currentPage * exercisesPerPage;
+    const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+    const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
     const paginate = (e, value) => {
         setCurrentPage(value);
@@ -39,6 +44,9 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
                 Showing Results
             </Typography>
             <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+                {currentExercises.map((exercise, index) => (
+                    <ExerciseCard key={index} exercise={exercise} />
+                ))}
             </Stack>
             <Stack mt="100px" alignItems="center">
                 {exercises.length > 9 && (
