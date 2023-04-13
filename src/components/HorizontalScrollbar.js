204ef0035418d512ecc301af091b0793029c537f
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-13 16:23:02 +0300

    Added ExerciseCard component

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -3,6 +3,7 @@ import { Box, Typography } from '@mui/material';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
 import BodyPart from './BodyPart';
+import ExerciseCard from './ExerciseCard';
 
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
@@ -40,7 +41,7 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
                     >
                         {isBodyParts
                             ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-                            :  
+                            : <ExerciseCard exercise={item} />
                         }
                     </Box>
                 )
