06de54ad4dd5bdc436641cec07dc5e067248d7e8
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-14 16:24:04 +0300

    Added Detail component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
 
 const ExerciseDetail = () => {
     const [exerciseDetail, setExerciseDetail] = useState({});
@@ -20,7 +21,9 @@ const ExerciseDetail = () => {
     }, [id]);
 
     return (
-        <Box></Box>
+        <Box>
+            <Detail exerciseDetail={exerciseDetail} />
+        </Box>
     );
 };
 
